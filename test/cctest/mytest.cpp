9e1cd8f6c8c15c10f419bb7f69a3a36732a3f448
hgl868 <hgl868@126.com>
2015-06-24 15:47:29 +0800

    添加NewPoint fun

--- test/cctest/mytest.cpp
+++ test/cctest/mytest.cpp
@@ -68,6 +68,29 @@ private:
 	int skeyid;
 };
   
+// This function returns a new array with three elements, x, y, and z.
+Handle<Array> NewPointArray(int x, int y, int z) 
+{
+	v8::Isolate* isolate = v8::Isolate::GetCurrent();
+
+	// We will be creating temporary handles so we use a handle scope.
+	HandleScope handle_scope(isolate); //声明了HandleScope了
+
+	// Create a new empty array.
+	Handle<Array> array = Array::New(3);
+
+	// Return an empty result if there was an error creating the array.
+	if (array.IsEmpty())
+		return Handle<Array>();
+
+	// Fill out the values
+	array->Set(0, Integer::New(x));
+	array->Set(1, Integer::New(y));
+	array->Set(2, Integer::New(z));
+	// Return the value through Close.
+	return handle_scope.Close(array); //这样才能正确返回一个本地handle
+}
+
 //向MakeWeak注册的callback.  
 void CloudAppWeakReferenceCallback(Persistent<Value> object, void * param)
 {  
